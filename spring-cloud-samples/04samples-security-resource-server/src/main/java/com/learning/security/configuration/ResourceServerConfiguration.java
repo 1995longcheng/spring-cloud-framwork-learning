b95764dabb332cd9cd34c8d4f0079a6c813cba86
xiongchaoqun <xiongchaoqun@gdcattsoft.com>
2019-07-25 09:29:45 +0800

    添加请求白名单和黑名单【通过设置HttpSecurity】

--- spring-cloud-samples/04samples-security-resource-server/src/main/java/com/learning/security/configuration/ResourceServerConfiguration.java
+++ spring-cloud-samples/04samples-security-resource-server/src/main/java/com/learning/security/configuration/ResourceServerConfiguration.java
@@ -1,14 +1,14 @@
 package com.learning.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 安全管理-资源服务器配置【依赖common组件后请开启EnableResourceServer注解】
@@ -18,43 +18,45 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
  */
 @Configuration
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-	
-	//Nacos Config 注入
-	@Value(value="${jwt.signKey}")
-	private String signKey;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private RemoteTokenServices remoteTokenServices;
 
 	/**
-	 * 1、使用JWT进行token校验 2、还可以使用RemoteTokenServices远程调用鉴权服务器
-	 * 
-	 * @return
+	 * 不需要拦截的 路径api-security
 	 */
-	@Bean
-	public TokenStore tokenStore() {
-		return new JwtTokenStore(accessTokenConverter());
-	}
-
-	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-		converter.setSigningKey(signKey);
-		return converter;
-	}
+	@Value("${security.basic.allowPath:/login*}")
+	String allowPath;
+	
+	@Value("${security.basic.filterPath:}")
+	String filterPath;
 
 	/**
 	 * 资源服务器相关配置
 	 */
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		resources.tokenStore(tokenStore());
+		this.remoteTokenServices.setRestTemplate(restTemplate);
+		super.configure(resources);
 	}
 
-	/**
-	 * HTTP请求相关配置
-	 */
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		String[] tmpAllowPath = allowPath == null ? new String[] { "/login" } : allowPath.split(",");
+		String[] tmpFilterPath = filterPath == null || "".equals(filterPath.trim()) ? null : filterPath.split(",");
+
+		if (tmpFilterPath == null || tmpFilterPath.length == 0) {
+			http.authorizeRequests().anyRequest().permitAll();// 所有的请求不认证
+		} else {
+			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().requestMatchers()
+					.anyRequest().and().anonymous().and()
+					.authorizeRequests().antMatchers(tmpAllowPath).permitAll()// 无需认证
+					.antMatchers(tmpFilterPath).authenticated(); // 配置访问权限控制，必须认证过后才可以访问
+
+		}
 	}
 
 }
